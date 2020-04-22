488a257f34cce641934d9559c2dc8db8b647c10c
Vladyslav <kytsevolov@gmail.com>
2020-04-21 19:30:04 -0600

    refactoring inprogress

--- client/src/context/AppReducer.js
+++ client/src/context/AppReducer.js
@@ -80,34 +80,4 @@ export default function (state, action) {
       error: action.payload.error
     };
   }
-
-  // }
-  // switch (action.type) {
-  //   case "DELETE_POST":
-  //     return {
-  //       ...posts,
-  //       current: posts.current.filter((post) => post._id !== action.payload)
-  //     };
-  //   case "ADD_POST":
-  //     return {
-  //       ...posts,
-  //       current: [...posts.current, action.payload],
-  //       errors: action.errors
-  //     };
-  //   case "GET_SINGLE_POST":
-  //     return {
-  //       ...posts,
-  //       post: action.payload
-  //     };
-  //   case "UPDATE_POST":
-  //     const update = posts.current.map((e) =>
-  //       e._id === action.id ? action.payload : e
-  //     );
-  //     return {
-  //       ...posts,
-  //       current: update
-  //     };
-  //   default:
-  //     return posts;
-  // }
 }
